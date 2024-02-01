95e76ace393784b57e9a867c1d8db2ead0f3ff5d
saimanoon <gurylev_andrei@list.ru>
2024-02-02 02:37:21 +0300

    Выполнено 3 задание

---
+++ lesson1/Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿System.Console.WriteLine("Введите число из отрезка [10, 99]");
+int x = int.Parse(Console.ReadLine()!);
+while (x < 10 || x > 99)
+{   
+    System.Console.WriteLine($"Число должно быть из отрезка [10, 99], введите повторно");
+    x = int.Parse(Console.ReadLine()!);
+}
+int digit_1 = x / 10;
+int digit_2 = x % 10;
+if (digit_1 > digit_2)
+{
+    System.Console.WriteLine($"Максимальная цифра {digit_1}");
+}
+else
+{
+    System.Console.WriteLine($"Максимальная цифра {digit_2}");
+}
